a9a3a97e4f830a982a4d5143f5a525a227fafd70
kenfil1972 <kenneth_palmer_72@yahoo.no>
2026-04-25 11:57:08 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-// WhiskyLog v1.33 - service worker disabled for stability
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.registration.unregister()));
