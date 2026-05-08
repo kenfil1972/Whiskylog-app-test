04e477ed3c1489cf4361d136429138882240615d
kenfil1972 <kenneth_palmer_72@yahoo.no>
2026-05-08 20:28:10 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# WhiskyLog v2.17 Opened Bottles Register Tasting
+
+Changes:
+- Under Åpnede flasker / Opened bottles, the Delete button is replaced by Register tasting.
+- Register tasting opens the tasting form with that bottle preselected.
+- Other categories still keep Delete.
---
+++ sw.js
@@ -0,0 +1,4 @@
+// WhiskyLog v2.17 - no persistent cache
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.registration.unregister())));
+self.addEventListener('fetch', e => {});
