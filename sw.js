c7c8573d29f41b0b1f179023422cbcb1b5522a17
kenfil1972 <kenneth_palmer_72@yahoo.no>
2026-05-08 17:08:52 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-// WhiskyLog v1.77 - cache kill during development
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(
-  caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))).then(() => self.registration.unregister())
-));
-self.addEventListener('fetch', e => {});
